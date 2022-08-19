bed044bbd1a39dee11fe586313363216c7282ed7
JMOgawa <joanaogawa@gmail.com>
2022-08-19 16:27:38 -0300

    DJ - 003

--- script/script.js
+++ script/script.js
@@ -1,31 +1,51 @@
-const user = { email: "joana@email.com", pass: "1234" };
+const user = { email: "joana@email.com", senha: "1234" };
 
+//SIGN IN
 function entrar() {
-    var nameUser = document.getElementById("name").value
-    console.log(nameUser)
-    var emailUser = document.getElementById("email").value
-    console.log(emailUser)
-    var senhaUser = document.getElementById("password").value
-    console.log(senhaUser)
-    console.log(user.pass + " - " + user.email)
-    if (emailUser != user.email || senhaUser != user.pass) {
-        alert("Senha ou email estão incorretos.")
+    var userNome = document.getElementById("name").value
+    console.log(userNome)
+    var userEmail = document.getElementById("email").value
+    console.log(userEmail)
+    var userSenha = document.getElementById("password").value
+    console.log(userSenha)
+    console.log(localStorage.getItem('senha') + " - " + localStorage.getItem('email'))
+    console.log(user.senha + " - " + user.email)
+
+    if (userEmail != localStorage.getItem('email') || userSenha != localStorage.getItem('senha')) {
+        if (userEmail != user.email || userSenha != user.senha) {
+            alert("Senha ou email estão incorretos.")
+        } else {
+            window.location.replace('accountPage.html')
+        }
     } else {
         window.location.replace('accountPage.html')
     }
 }
 
 
-function signup(user){
+//SIGN UP
+function signup() {
     var nomeUser = document.getElementById("nome").value
     console.log(nomeUser)
-    var passUser1 = document.getElementById("senha").value
-    var passUser2 = document.getElementById("confSenha").value
-    console.log(passUser1)
-    var userEmail = document.getElementById("email").value
-    console.log(userEmail)
+    var senhaUser1 = document.getElementById("senha").value
+    var senhaUser2 = document.getElementById("confSenha").value
+    console.log(senhaUser1)
+    var emailUser = document.getElementById("email").value
+    console.log(emailUser)
 
-    if(){
-        
+    if (senhaUser1 != senhaUser2) {
+        alert("Erro na confirmação de senhas. Verifique sua senha.")
+    } else {
+        localStorage.setItem('email', emailUser)
+        console.log(localStorage.getItem('email'))
+        localStorage.setItem('senha', senhaUser1)
+        console.log(localStorage.getItem('senha'))
+        window.location.replace('index.html')
     }
+}
+
+
+//MUDAR O TEMA - DARK MODE
+function darkmode(){
+
 }
